acdb118b20e03b19fff5dd62144216b212aeb23d
Liou <liou.wang.it@gmail.com>
2023-01-05 09:24:16 +0100

    Create Layout component and update index file and global styles

---
+++ components/Layout.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+
+export type LayoutProps = {
+  children: React.ReactNode;
+  home?: boolean;
+};
+
+const Layout = ({ children, home }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        {/* Move Title out*/}
+        <meta name='og:title' content='Liou Wang - Front-end Developer' />
+        <title>Liou Wang - Front-end Developer</title>
+        <meta
+          name='description'
+          content='Liou Wang is a Frontend Developer (React / NextJs / TypeScript)'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <main className='w-4/5 md:w-3/5 mx-auto mt-10'>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
