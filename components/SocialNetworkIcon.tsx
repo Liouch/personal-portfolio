0fb5fa79b8075a9acf6a2a9c76762dc6eb78e07e
Liou <liou.wang.it@gmail.com>
2023-01-09 11:20:24 +0100

    Create reusable component to display icon and link to social network

---
+++ components/SocialNetworkIcon.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
+type Props = {
+  socialNetwork: 'Linkedin' | 'Github' | 'Email';
+  size?: 'small' | 'normal' | 'large';
+};
+
+const SocialNetworkIcon = ({ socialNetwork, size = 'normal' }: Props) => {
+  const iconUrl: Record<Props['socialNetwork'], string> = {
+    Linkedin: 'https://www.linkedin.com/in/liou-wang-/',
+    Github: 'https://github.com/Liouch',
+    Email: 'mailto:liou.wang.it@gmail.com',
+  };
+  const iconSize: Record<NonNullable<Props['size']>, string> = {
+    small: '1rem',
+    normal: '1.5rem',
+    large: '2rem',
+  };
+  const icon: Record<Props['socialNetwork'], JSX.Element> = {
+    Linkedin: <FaLinkedinIn size={iconSize[size]} />,
+    Github: <FaGithub size={iconSize[size]} />,
+    Email: <AiOutlineMail size={iconSize[size]} />,
+  };
+  return (
+    <div className='flex justify-center items-center'>
+      <a href={iconUrl[socialNetwork]} target='_blank' rel='noreferrer' className='rounded-full shadow-md shadow-gray-400 p-3 hover:scale-110 ease-in duration-500'>
+        {icon[socialNetwork]}
+      </a>
+    </div>
+  );
+};
+
+export default SocialNetworkIcon;
